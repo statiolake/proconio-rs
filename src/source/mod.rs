3dacb8dfee190cc894c63a3e099bd848350ce0d0
statiolake <statiolake@gmail.com>
2019-06-01 11:42:22 +0900

    (wip) Moving current source into once

---
+++ src/source/mod.rs
@@ -0,0 +1,19 @@
+pub mod line;
+pub mod once;
+
+// TODO: this should be inverted
+#[cfg(not(debug_assertions))]
+pub use self::line::Source;
+#[cfg(debug_assertions)]
+pub use self::once::Source;
+
+/// A trait representing which type can be read from `Source`.
+///
+/// If you want to read your own type using `input!`, you can implement this
+/// trait for your type.  Alternatively, you can add `#[derive(ReadSource)]` if
+/// you put `use proconio_derive::ReadSource` in your source.  It automatically
+/// implements `ReadSource` if all members of your type are `ReadSource`.
+pub trait ReadSource {
+    type Output;
+    fn read(source: &mut Source) -> Self::Output;
+}
--- src/source/once.rs
+++ src/source/once.rs
@@ -24,14 +24,3 @@ impl Source<'_> {
         self.tokens.next()
     }
 }
-
-/// A trait representing which type can be read from `Source`.
-///
-/// If you want to read your own type using `input!`, you can implement this
-/// trait for your type.  Alternatively, you can add `#[derive(ReadSource)]` if
-/// you put `use proconio_derive::ReadSource` in your source.  It automatically
-/// implements `ReadSource` if all members of your type are `ReadSource`.
-pub trait ReadSource {
-    type Output;
-    fn read(source: &mut Source) -> Self::Output;
-}
