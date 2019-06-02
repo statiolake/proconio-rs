88b76435923e0710848caaf0a25db2819b876e08
statiolake <statiolake@gmail.com>
2019-06-03 03:37:18 +0900

    Refactor proconio-derive

--- proconio-derive/src/lib.rs
+++ proconio-derive/src/lib.rs
@@ -10,51 +10,129 @@ use syn::{Data, DeriveInput, Fields};
 pub fn read_derive(input: TokenStream) -> TokenStream {
     let ast: DeriveInput = syn::parse(input).expect("failed to parse input.");
 
-    let name = &ast.ident;
+    let name = get_name(&ast);
+    let fields = get_data(ast).fields;
+
+    let field_info = field_info(&fields);
+    let decl = declare(&fields, &name, &field_info);
+    let generate = generate(&fields, &name, &field_info);
+    let reads = field_info.iter().map(|f| &f.read);
+
+    let res = quote! {
+        impl proconio::source::ReadSource for #name {
+            type Output = #name;
+            fn read<R: std::io::BufRead>(source: &mut proconio::source::Source<R>) -> #name {
+                #(#reads)*
+                #generate
+            }
+        }
+    };
+    res.into()
+}
+
+fn get_name(ast: &syn::DeriveInput) -> syn::Ident {
+    ast.ident.clone()
+}
+
+fn get_data(ast: syn::DeriveInput) -> syn::DataStruct {
     let data = ast.data;
-    let data = match data {
+
+    match data {
         Data::Struct(data) => data,
         _ => panic!("ReadSource can only derivable for structs."),
-    };
-    let fields = data.fields;
+    }
+}
 
-    let (idents, read) = match fields {
-        Fields::Named(_) => fields
-            .iter()
-            .map(|field| {
-                let ident = field
-                    .ident
-                    .as_ref()
-                    .cloned()
-                    .expect("internal error: named field doesn't have name");
-                let ty = &field.ty;
-                (
-                    ident.clone(),
-                    quote! {
-                        let #ident = <#ty as proconio::source::ReadSource>::read(source);
-                    },
-                )
-            })
-            .unzip(),
-        Fields::Unnamed(_) => fields
-            .iter()
-            .enumerate()
-            .map(|(idx, field)| {
-                let ident = format!("field{}", idx);
-                let ident = syn::Ident::new(&ident, Span::call_site());
-                let ty = &field.ty;
-                (
-                    ident.clone(),
-                    quote! {
-                        let #ident = <#ty as proconio::source::ReadSource>::read(source);
-                    },
-                )
-            })
-            .unzip(),
-        Fields::Unit => (Vec::new(), Vec::new()),
-    };
+struct FieldInfo {
+    ident: syn::Ident,
+    ty: syn::Type,
+    read: proc_macro2::TokenStream,
+}
 
-    let generate = match fields {
+fn field_info(fields: &syn::Fields) -> Vec<FieldInfo> {
+    match fields {
+        Fields::Named(_) => field_named(fields),
+        Fields::Unnamed(_) => field_unnamed(fields),
+        Fields::Unit => Vec::new(),
+    }
+}
+
+fn field_named(fields: &syn::Fields) -> Vec<FieldInfo> {
+    let mut res = Vec::new();
+
+    for field in fields {
+        let ident = field.ident.as_ref().cloned();
+        let ident = ident.expect("internal error: named field doesn't have name");
+        let ty = field.ty.clone();
+        let read = quote! {
+            let #ident = <#ty as proconio::source::ReadSource>::read(source);
+        };
+
+        res.push(FieldInfo { ident, ty, read });
+    }
+
+    res
+}
+
+fn field_unnamed(fields: &syn::Fields) -> Vec<FieldInfo> {
+    let mut res = Vec::new();
+
+    for (idx, field) in fields.iter().enumerate() {
+        let ident = format!("field{}", idx);
+        let ident = syn::Ident::new(&ident, Span::call_site());
+        let ty = field.ty.clone();
+        let read = quote! {
+            let #ident = <#ty as proconio::source::ReadSource>::read(source);
+        };
+
+        res.push(FieldInfo { ident, ty, read });
+    }
+
+    res
+}
+
+fn declare(
+    fields: &syn::Fields,
+    name: &syn::Ident,
+    field_info: &[FieldInfo],
+) -> proc_macro2::TokenStream {
+    let idents = field_info.iter().map(|f| &f.ident);
+    let types = field_info.iter().map(|f| &f.ty);
+
+    match fields {
+        Fields::Named(_) => {
+            quote! {
+                #[derive(Debug)]
+                struct #name {
+                    #(#idents: #types,)*
+                }
+            }
+        }
+
+        Fields::Unnamed(_) => {
+            quote! {
+                #[derive(Debug)]
+                struct #name(#(#idents,)*);
+            }
+        }
+
+        Fields::Unit => {
+            quote! {
+                #[derive(Debug)]
+                struct #name;
+            }
+        }
+    }
+}
+
+fn generate(
+    fields: &syn::Fields,
+    name: &syn::Ident,
+    field_info: &[FieldInfo],
+) -> proc_macro2::TokenStream {
+    let idents = field_info.iter().map(|f| &f.ident);
+
+    match fields {
         Fields::Named(_) => {
             quote! {
                 #name {
@@ -62,35 +140,17 @@ pub fn read_derive(input: TokenStream) -> TokenStream {
                 }
             }
         }
+
         Fields::Unnamed(_) => {
             quote! {
                 #name(#(#idents,)*)
             }
         }
+
         Fields::Unit => {
             quote! {
                 #name
             }
         }
-    };
-
-    let gen = quote! {
-        impl proconio::source::ReadSource for #name {
-            type Output = #name;
-            fn read<R: std::io::BufRead>(source: &mut proconio::source::Source<R>) -> #name {
-                #(#read)*
-                #generate
-            }
-        }
-    };
-
-    gen.into()
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
     }
 }
