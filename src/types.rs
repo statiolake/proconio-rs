3d9ae5777cc073c019d12630a47f63a9ce6c6793
statiolake <statiolake@gmail.com>
2019-06-20 18:03:31 +0900

    Add document for types

--- src/types.rs
+++ src/types.rs
@@ -1,8 +1,15 @@
+//! Declares special types and aliases.
+
+/// Chars: read a string as array of chars.
 pub type Chars = Vec<char>;
+
+/// Bytes: read a string as array of bytes.
 pub type Bytes = Vec<u8>;
 
+/// Usize1: 1-indexed usize.  Output of reading has type usize.
 #[derive(Debug)]
 pub struct Usize1;
 
+/// Isize1: 1-indexed isize.  Output of reading has type isize.
 #[derive(Debug)]
 pub struct Isize1;
