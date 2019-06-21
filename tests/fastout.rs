0b81a1a0b729b7a3661faee846590e4f0d1f2f3d
statiolake <statiolake@gmail.com>
2019-06-21 16:52:22 +0900

    Fix test for #[fastout]

--- tests/fastout.rs
+++ tests/fastout.rs
@@ -9,6 +9,9 @@ fn foo() -> i32 {
 #[fastout]
 #[test]
 fn main() {
-    ::print!("hello, world!");
+    print!("hello, world!");
     std::println!("{}", foo());
+    print!("{}{}, ", 'h', "ello"); // "hello"       (no newline)
+    println!("{}!", "world"); // "world!\n"
+    println!("{}", 123456789); // "123456789\n"
 }
